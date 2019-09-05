a51a522dc2135beda5c217824c3bf255e9b50850
Tristan Bowler <33989178+tristanbowler@users.noreply.github.com>
2019-09-04 22:36:08 -0600

    Add project to repo

---
+++ TeamB_Prototype01/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody myRigidBody;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed; 
+    }
+
+    void FixedUpdate()
+    {
+        myRigidBody.velocity = moveVelocity;
+    }
+}
