d14d529712f11326cb051443ff9fc0de76067b19
Tristan Bowler <33989178+tristanbowler@users.noreply.github.com>
2019-09-07 18:09:05 -0600

    Create movement mechanic. Add sample crowd, barriers, and lights.

---
+++ TeamB_Prototype01/Assets/Scripts/CrowdController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrowdController : MonoBehaviour
+{
+
+    private GameObject player;
+    private Vector3 prevPlayerPos;
+    private Vector3 newPlayerPos;
+    public int rotationSpeed;
+    public int moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        prevPlayerPos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        newPlayerPos = player.transform.position;
+        if(newPlayerPos != prevPlayerPos)
+        {
+            Vector3 direction = newPlayerPos - transform.position;
+            direction.y = 0.0f;
+            double distance = Mathf.Sqrt(Mathf.Pow((direction.x - newPlayerPos.x), 2) + Mathf.Pow((direction.y - newPlayerPos.y), 2) + Mathf.Pow((direction.z - newPlayerPos.z), 2));
+            Debug.Log("DISTANCE: " + distance);
+            if ((direction != Vector3.zero) && (distance > 4.5))
+            {
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(Vector3.right, direction), rotationSpeed * Time.deltaTime);
+                transform.position += (newPlayerPos - transform.position).normalized * moveSpeed * Time.deltaTime;
+
+            }
+            else
+            {
+                
+                SceneManager.LoadScene(0);
+            }
+            prevPlayerPos = newPlayerPos;
+            
+        }
+        
+        
+    }
+}
--- TeamB_Prototype01/Assets/Scripts/PlayerController.cs
+++ TeamB_Prototype01/Assets/Scripts/PlayerController.cs
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        myRigidBody = GetComponent<Rigidbody>();
+        myRigidBody = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
